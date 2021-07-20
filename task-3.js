23b8493d13f1881b69bc063771cf6e634115b391
AndriyKh1992 <andriykhoruzhyi@gmail.com>
2021-07-20 23:52:17 +0300

    second commit

--- task-3.js
+++ task-3.js
@@ -1,4 +1,13 @@
 let message;
 const ADMIN_PASSWORD = 'jqueryismyjam';
-prompt("Password");
-if ()
+let password = prompt("Password");
+if (password === null) {
+    message = "'Скасовано користувачем!'";
+}
+else if (password === ADMIN_PASSWORD) {
+    message = "'Ласкаво просимо!'";
+}
+else {
+    message = "'Доступ заборонений, невірний пароль!'";
+}
+alert(message);
